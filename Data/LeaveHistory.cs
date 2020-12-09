11a61878bef538328288741ccea2c0f845f6694e
Kayla Ziegler <kaydanzie@gmail.com>
2020-12-09 14:59:13 -0500

    Adds the 3 new Leave detail database tables

--- Data/LeaveHistory.cs
+++ Data/LeaveHistory.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace leave_management.Data
 {
     public class LeaveHistory
     {
-        public LeaveHistory()
-        {
-        }
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("RequestingEmployeeId")]
+        public Employee RequestingEmployee { get; set; }
+        public string RequestingEmployeeId { get; set; }
+
+        [ForeignKey("ApprovedById")]
+        public Employee ApprovedBy { get; set; }
+        public string ApprovedById { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime DateRequested { get; set; }
+        public DateTime DateActioned { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public bool? Approved { get; set; }
     }
 }
--- Data/LeaveType.cs
+++ Data/LeaveType.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace leave_management.Data
 {
     public class LeaveType
     {
-        public LeaveType()
-        {
-        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
     }
 }
