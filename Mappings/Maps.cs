4d5440356634a94f6f47acff25592aa5369aec52
Kayla Ziegler <kaydanzie@gmail.com>
2020-12-10 11:42:53 -0500

    Adds auto mapping for view models

---
+++ Mappings/Maps.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AutoMapper;
+using leave_management.Data;
+using leave_management.Models;
+
+namespace leave_management.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            // Use ReverseMap so you don't need to add CreateMap<DetailsLeaveTypeVM, LeaveType>
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+        }
+    }
+}
---
+++ Models/LeaveHistoryVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace leave_management.Models
+{
+    public class LeaveHistoryVM
+    {
+
+        public int Id { get; set; }
+
+
+        public EmployeeVM RequestingEmployee { get; set; }
+        public string RequestingEmployeeId { get; set; }
+
+
+        public EmployeeVM ApprovedBy { get; set; }
+        public string ApprovedById { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+        public DateTime DateRequested { get; set; }
+        public DateTime DateActioned { get; set; }
+
+        public DetailsLeaveTypeVM LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+
+        public bool? Approved { get; set; }
+    }
+}
