6abfd8a3e4dca36bb7d6b7a9c96ffab0ac2fda58
Kayla Ziegler <kaydanzie@gmail.com>
2020-12-11 13:50:46 -0500

    Adds first and last name to registration form

---
+++ Areas/Identity/Pages/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace leave_management.Areas.Identity.Pages
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
