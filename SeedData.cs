2258e502f72e7d3fc9e48907db8a8a54051f7582
Kayla Ziegler <kaydanzie@gmail.com>
2020-12-11 16:09:30 -0500

    Adds seed user data

---
+++ SeedData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace leave_management
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> manager, RoleManager<IdentityRole> roleManager)
+        {
+            // Roles need to be created first
+            SeedRoles(roleManager);
+            SeedUsers(manager);
+        }
+
+        private static void SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if(userManager.FindByNameAsync("admin").Result == null) {
+                var user = new IdentityUser { UserName = "admin@localhost.com", Email = "admin@localhost.com" };
+                var result = userManager.CreateAsync(user, "Password1@").Result;
+
+                if(result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+        }
+
+        private static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if(!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                var role = new IdentityRole { Name = "Administrator" };
+                var result = roleManager.CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("Employee").Result)
+            {
+                var role = new IdentityRole { Name = "Employee" };
+                var result = roleManager.CreateAsync(role);
+            }
+        }
+    }
+}
